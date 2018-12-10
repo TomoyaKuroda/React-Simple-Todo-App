e3ea7115fabcbf1831cd0e8e7a539d4c916bf6f4
Tomoya <200378857@student.georgianc.on.ca>
2018-12-10 00:53:19 -0500

    add background image

---
+++ .history/src/components/TodoList_20181210001952.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
+export default class TodoList extends Component {
+  render() {
+    const {items,handleDelete,handleEdit}=this.props
+    return (
+      <Paper>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Item</TableCell>
+              <TableCell>Due Date</TableCell>
+              <TableCell>Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+{
+  items.map(item => {
+    return <TodoItem key={item.id} title={item.title} date={item.date} editItem={item.editItem} handleDelete={() => handleDelete(item.id)} handleEdit={() => handleEdit(item.id)} />;
+  })
+}
+         </TableBody>
+        </Table>
+      </Paper>
+    )
+  }
+}
